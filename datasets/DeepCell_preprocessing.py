76768aa9df1f27db0f3eea9c217ce1f10a7267a4
unknown <siwoonam@dgist.ac.kr>
2024-07-08 23:50:06 -0400

    train_test

--- datasets/DeepCell_preprocessing.py
+++ datasets/DeepCell_preprocessing.py
@@ -6,13 +6,18 @@ import skimage.io as io
 ### Load the test split
 
 npz_dir = '/media/NAS/nas_70/open_dataset/DeepCell'
-test_dict = np.load(os.path.join(npz_dir, 'tissuenet_v1.1_train.npz'))
-
+train_dict = np.load(os.path.join(npz_dir, 'tissuenet_v1.1_train.npz'))
+val_dict = np.load(os.path.join(npz_dir, 'tissuenet_v1.1_val.npz'))
+test_dict = np.load(os.path.join(npz_dir, 'tissuenet_v1.1_test.npz'))
 ### Get the image data from the npz
 
+train_X, train_y = test_dict['X'], test_dict['y']
+val_X, val_y = test_dict['X'], test_dict['y']
 test_X, test_y = test_dict['X'], test_dict['y']
 
 ### Create overlays of image data and labels
 
-print(test_X.shape)
-print(test_y.shape)
+print(train_X.shape, train_y.shape)
+print(val_X.shape, val_y.shape)
+print(test_X.shape, test_y.shape)
+
