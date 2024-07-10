bf8dc05cfdba3dbf92d2b03044d0aebe1934465e
unknown <siwoonam@dgist.ac.kr>
2024-07-10 17:38:42 -0400

    train_test

--- models/sam.py
+++ models/sam.py
@@ -283,6 +283,7 @@ class SAM(nn.Module):
                             gt_local_part, gt_global_part = self.make_pseudo_instance_map(b, point_coord[num_p: ], point_label[num_p: ], x_ori[b].unsqueeze(0))
 
                         else:
+                            print(point_coord.shape, point_label.shape, point_coord[num_p: ].shape, point_label[num_p: ].shape)
                             gt_local_part, gt_global_part = self.make_pseudo_instance_map(b, point_coord[num_p: num_p+20], point_label[num_p: num_p+20], x_ori[b].unsqueeze(0))
 
                         gt_local_part = gt_local_part + num_p
