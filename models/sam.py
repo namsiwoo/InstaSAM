aec7811b1a390fe9bb99f92e26960b2fbce6727a
unknown <siwoonam@dgist.ac.kr>
2024-07-11 00:33:03 -0400

    train_test

--- models/sam.py
+++ models/sam.py
@@ -279,9 +279,9 @@ class SAM(nn.Module):
         for b in range(len(points)):
             if len(torch.unique(points[0])) > 1:
                 point_coord, point_label = make_point_prompt(points[b], only_fg=False)
-                if torch.sum(points[b]) > 100:
+                if torch.sum(points[b]) > 300:
                     mask_prompt_adapter = torch.zeros_like(points.squeeze(1)).to(self.device)
-                    gt_local, gt_global = torch.zeros(1, 224, 224).to(self.device)-1, torch.zeros(1, 224, 224).to(self.device)-1
+                    gt_local, gt_global = (torch.zeros(1, 224, 224).to(self.device)-1).long(), (torch.zeros(1, 224, 224).to(self.device)-1).long()
 
 
                     # gt_local, gt_global = torch.zeros(1, 224, 224).to(self.device), torch.zeros(1, 224, 224).to(self.device)
