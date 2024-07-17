824500edc9f9f6ea527abec22b834ea424eb6729
unknown <siwoonam@dgist.ac.kr>
2024-07-17 18:37:20 -0400

    train_test

--- models/sam.py
+++ models/sam.py
@@ -234,6 +234,7 @@ class SAM(nn.Module):
         self.input = F.interpolate(self.input, (self.inp_size, self.inp_size), mode='bilinear', align_corners=True)
         if gt_mask is not None:
             self.gt_mask = gt_mask.to(self.device)
+            print(self.gt_mask.shape)
             plt.imshow(self.gt_mask.detach().cpu().numpy()[0].transpose(1, 2, 0))
             plt.colorbar()
             plt.savefig('/media/NAS/nas_187/siwoo/2024/revision/MO_test_sup/ex.png')
