0ec502378742b747fee9b665290fd8c2b614dbec
unknown <siwoonam@dgist.ac.kr>
2024-07-25 15:30:39 -0400

    train_test

--- datasets/MoNuSeg_dataset.py
+++ datasets/MoNuSeg_dataset.py
@@ -268,6 +268,7 @@ class Crop_dataset(torch.utils.data.Dataset): #MO, CPM, CoNSeP
                     box_label = np.array(Image.open(os.path.join(self.root_dir, 'labels_instance', self.split, img_name[:-4] + '_label.png')))
 
                 box_label = skimage.morphology.label(box_label)
+                print(np.unique(box_label))
                 box_label = Image.fromarray(box_label.astype(np.uint16))
 
                 sample = [img, box_label]#, cluster_label, voronoi_label]  # , new_mask
