3a7905ad2ee7ff5a3143de4c72e8cfcc8233c39b
unknown <siwoonam@dgist.ac.kr>
2024-08-01 18:31:18 -0400

    train_test

--- datasets/SegPC_preprocessing.py
+++ datasets/SegPC_preprocessing.py
@@ -94,6 +94,8 @@ def split_patches_label(data_dir, img_name_list, label_list, save_dir, patch_siz
             idx = 0
             while True:
                 image_name_idx = os.path.join(data_dir, image_name[:-4]+'_'+str(idx)+'.bmp')
+                print(image_name_idx)
+                print('-- ', label_list)
                 if image_name_idx in label_list:
                     pass
                 else:
