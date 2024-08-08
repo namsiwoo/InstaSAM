f8c98d557bd2f0bc210a03c6d8044c9b28747e7b
unknown <siwoonam@dgist.ac.kr>
2024-08-08 19:36:36 -0400

    train_test

--- datasets/MoNuSeg_dataset.py
+++ datasets/MoNuSeg_dataset.py
@@ -106,7 +106,8 @@ class gt_with_weak_dataset(torch.utils.data.Dataset):
                 point = Image.fromarray(point)
 
 
-                if img_name[:-5] == '7':
+                # if img_name[:-5] == '7':
+                if img_name[-7:-5] == '2_2':
                     box_label = np.array(Image.open(os.path.join(self.root_dir, 'labels_instance', self.split, img_name[:-4]+'_label.png')))
                     box_label = skimage.morphology.label(box_label)
                     box_label = Image.fromarray(box_label.astype(np.uint16))
--- models/sam.py
+++ models/sam.py
@@ -532,7 +532,8 @@ class SAM(nn.Module):
 
             else:
                 _, global_gt = self.forward_ssl(point_prompt, img_name, epoch)
-                if img_name[-5] != '7':
+                # if img_name[-5] != '7':
+                if img_name[-7:-5] == '2_2':
                     bce_loss, offset_loss, iou_loss, offset_gt = self.backward_G_ssl(global_gt)
                     bce_loss_local, iou_loss_local = 0, 0
                 else:
