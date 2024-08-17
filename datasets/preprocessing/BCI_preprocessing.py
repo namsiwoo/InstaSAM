69c8ed89e9848d8bbc2504baf1f0797dfdda486f
unknown <siwoonam@dgist.ac.kr>
2024-08-17 16:01:33 -0400

    train_test

---
+++ datasets/preprocessing/BCI_preprocessing.py
@@ -0,0 +1,7 @@
+import h5py
+
+# 읽기 모드로 파일 열기
+with h5py.File('/media/NAS/nas_70/open_dataset/BCI_dataset/BCData/annotations/test/positive/0.h5', 'r') as f:
+    # 데이터셋 로드
+    data = f['my_dataset'][:]
+    print(data)
