7ffc1bc99d4cf0955b14b9f40e7ce408cded6c72
unknown <siwoonam@dgist.ac.kr>
2024-08-20 17:57:54 -0400

    train_test

--- datasets/preprocessing/DeepLIIF_preprocessing.py
+++ datasets/preprocessing/DeepLIIF_preprocessing.py
@@ -46,8 +46,8 @@ if __name__ == '__main__':
             for i in range(len(img_classes)):
                 crop_img = img[:, i*img_size: (i+1)*img_size, :]
                 if i == 5:
-                    positive = (crop_img[:, :, 0] == 1).astype(np.uint8)*255
-                    negative = (crop_img[:, :, 2] == 1).astype(np.uint8)*255
+                    positive = (crop_img[:, :, 0] != 0).astype(np.uint8)*255
+                    negative = (crop_img[:, :, 2] != 0).astype(np.uint8)*255
                     instance = (np.sum(crop_img, axis=2)>0).astype(np.uint8)
                     instance, _, _ = make_instance_sonnet(instance, positive+negative)
                     positive = Image.fromarray(positive.astype('uint8')).convert('L')
