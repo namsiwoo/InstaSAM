e415ad3f53a362c730c438bf13caab4615aa922e
unknown <siwoonam@dgist.ac.kr>
2024-09-09 01:21:14 -0400

    train_test

--- models/sam_DA.py
+++ models/sam_DA.py
@@ -588,7 +588,7 @@ class SAM(nn.Module):
         #2
         else:
             dis_gt = self.netD_mask(self.gt_mask.float())
-            dis_offset = self.netD_offset(offset_gt.float())
+            dis_offset = self.netD_offset(offset_gt.unsqueeze(0).float())
             space_query1 = self.netD_mask(self.pred_mask.detach())
             space_query2 = self.netD_mask(self.pred_mask2.detach())
             channel_query1 = self.netD_offset(self.masks_hq.detach())
