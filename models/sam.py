07eb6ca385c0821eff1795f1aabffe46f71b6192
unknown <siwoonam@dgist.ac.kr>
2024-09-14 23:20:55 -0400

    first commit

--- models/sam.py
+++ models/sam.py
@@ -495,7 +495,7 @@ class SAM(nn.Module):
             feature_loss += loss_ssc
         else:
             print("loss_ssc is NaN!")
-            loss_ssc = torch.zeros_like(index_)
+            loss_ssc = torch.zeros_like(loss_ssc)
 
         return loss_ssc
 
