3471af36bd22e8745646ad6cf86152679c0fef84
unknown <siwoonam@dgist.ac.kr>
2024-09-19 13:40:30 -0400

    first commit

--- models/sam.py
+++ models/sam.py
@@ -281,7 +281,7 @@ class SAM(nn.Module):
             # if len(torch.unique(points[0])) > 1:
             if torch.sum(points[b]) > 0:
                 point_coord, point_label = make_point_prompt(points[b], only_fg=False)
-                print(len(torch.unique(points[0])))
+                # print(len(torch.unique(points[0])))
                 if len(torch.unique(points[0])) > 10000000: #cut
                     mask_prompt_adapter = torch.zeros_like(points.squeeze(1)).to(self.device).float()
 
