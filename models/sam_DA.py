374a7990ec344c246582e0c74be6bb40598f14e1
unknown <siwoonam@dgist.ac.kr>
2024-09-27 19:15:25 -0400

    train_test

--- models/sam_DA.py
+++ models/sam_DA.py
@@ -648,13 +648,13 @@ class SAM(nn.Module):
         bce_loss, offset_loss, iou_loss, offset_gt = self.backward_G()  # calculate graidents for G
         space_loss, channel_loss = self.backward_G_dis(offset_gt)
         recon_loss = self.backward_recon()
-        self.loss_G = bce_loss + iou_loss + offset_loss + recon_loss
-        # if self.type ==3:
-        #     self.loss_G = bce_loss + iou_loss + offset_loss + space_loss[0] + space_loss[1] + channel_loss[0] + channel_loss[1] + recon_loss
-        #     # space_loss = space_loss[0]
-        #     # channel_loss = channel_loss[0]
-        # else:
-        #     self.loss_G = bce_loss + iou_loss + offset_loss +space_loss + channel_loss
+        # self.loss_G = bce_loss + iou_loss + offset_loss + recon_loss
+        if self.type ==3:
+            self.loss_G = bce_loss + iou_loss + offset_loss + space_loss[0] + space_loss[1] + channel_loss[0] + channel_loss[1] + recon_loss
+            # space_loss = space_loss[0]
+            # channel_loss = channel_loss[0]
+        else:
+            self.loss_G = bce_loss + iou_loss + offset_loss +space_loss + channel_loss
 
         self.optimizer.zero_grad()  # set G's gradients to zero
         self.loss_G.backward()
@@ -663,7 +663,7 @@ class SAM(nn.Module):
         del self.loss_G, self.pred_mask2, self.masks_hq2
         # train discriminator....
 
-        if epoch > 1000 == 0:
+        if epoch %  5 == 0:
             self.forward()
             space_loss, channel_loss = self.backward_G_dis(offset_gt)
 
